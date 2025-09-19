3538107d73fc75abf3f34b52b136eac3bc2b2ea4
trizDamacena <beatrizfdamacena@gmail.com>
2025-09-19 16:54:14 -0300

    Fazer cadastro

--- server.py
+++ server.py
@@ -14,6 +14,10 @@
 
 import os 
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
+
+usuario = ""
+senhaUsuario = ""
 
 class Handler(SimpleHTTPRequestHandler):
     def list_directory(self, path):
@@ -28,6 +32,18 @@ class Handler(SimpleHTTPRequestHandler):
         except FileNotFoundError:
             pass 
         return super().list_directory(path)
+    
+    def accont_user(self, login, senhaa):
+        self.login = login
+        self.senhaa = senhaa
+
+        loga = "beatriz"
+        senha = 1234
+
+        if (self.login == loga and self.senhaa == senha):
+            return "Usuário logado"
+        else: 
+            return "Usuário não encontrado"
     
     def do_GET(self): #definindo o método get para o endpoint indicados abaixo
         if self.path == "/index": #realizando o get da página index pelo endpoint /index 
@@ -75,7 +91,49 @@ class Handler(SimpleHTTPRequestHandler):
                 self.send_error(404, "File Not Found")
         else:
             super().do_GET()
-                
+    
+    def do_POST(self):
+        
+        if self.path == '/send_login':
+            content_length = int(self.headers['Content-length']) #tamanho do conteúdo
+            body = self.rfile.read(content_length).decode('utf-8')
+            form_data = parse_qs(body) 
+
+            print("Data Form:") 
+            print("Usuario: ", form_data.get('nome_usuario', [""])[0])         
+            print("Password: ", form_data.get('senha', [""])[0])    
+
+            username = form_data.get('nome_usuario', [""])[0]
+            senhauser= int(form_data.get('senha', [""])[0])
+
+            logou = self.accont_user(username, senhauser)
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(logou.encode("utf-8"))
+        
+
+        elif (self.path == '/send_cadastro'):
+            content_length = int(self.headers['Content-length'])
+            body = self.rfile.read(content_length).decode('utf-8')
+            form_data = parse_qs(body)
+
+            print("Data frame: ")
+            nome_filme = form_data.get('nome_filme', [""])[0]
+            atores = form_data.get('atores', [""])[0]
+            diretor = form_data.get('diretor', [""])[0]
+            data_lancamento = form_data.get('data_lancamento', [""])[0]
+            genero = form_data.get('genero', [""])[0]
+            produtora = form_data.get('produtora', [""])[0]
+            sinopse = form_data.get('sinopse', [""])[0]
+            print(nome_filme)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(logou.encode("utf-8"))
+        else: 
+            super(Handler, self).do_POST()
 
 porta = 8000 # definindo a porta
 Hanlder = (f'http://localhost:{porta}') #definindo o endpoint 
